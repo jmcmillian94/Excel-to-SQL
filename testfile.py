9a51671a4b0c7ab01cd9ef951227ad698ae721aa
jmcmillian94 <jmcmillian94@aim>
2024-12-13 12:53:24 -0500

    csv functionality added

--- testfile.py
+++ testfile.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import sqlite3
 import os
+import time
 
 # define function needed to convert excel file to SQL table
 def convertExcelToSQL(fileToConvert,dbName,tableName):
@@ -39,17 +40,20 @@ def convertCSVToSQL(fileToConvert,dbName,tableName):
 
 # main execution block
 if __name__ == "__main__":
+    print('SQLite Database Creation Tool')
+    print("\nThis program will take any Excel or CSV file and convert it to a table for the specified SQLite database.")
+    time.sleep(3)
     while True:
-        choice = input("Are you using an excel file(1) or a csv file(2)?")
+        choice = input("\nPlease select the file type you are using by typing the corresponding number or type 'q' to quit the program\n \n 1.) Excel\n 2.) CSV\n\n=>")
         try:
             if choice == '1':
                 fileToConvert = input("Enter the path of the Excel file (including .xlsx extension): ")
                 # Validate if the file exists
                 if not os.path.exists(fileToConvert):
-                    print("Error: The specified Excel file does not exist. Please check the file path.")
+                    print("Error: The specified Excel file does not exist. Please check the file path. Returning to start...")
                 else:
-                    dbName = input("Enter the name for the SQL database (without extension): ")
-                    tableName = input("Enter the name for the SQL table:")
+                    dbName = input("Enter the name for the SQLite database (without extension) to be created or modified: ")
+                    tableName = input("Enter the name for the SQL table. An existing table with the same name will be overwritten in the database:")
                     convertExcelToSQL(fileToConvert, dbName,tableName)
             if choice == '2':
                 fileToConvert = input("Enter the path of the csv file (including .csv extension): ")
@@ -57,11 +61,13 @@ if __name__ == "__main__":
                 if not os.path.exists(fileToConvert):
                     print("Error: The specified CSV file does not exist. Please check the file path.")
                 else:
-                    dbName = input("Enter the name for the SQL database (without extension): ")
-                    tableName = input("Enter the name for the SQL table:")
+                    dbName = input("Enter the name for the SQLite database (without extension) to be created or modified: ")
+                    tableName = input("Enter the name for the SQL table. An existing table with the same name will be overwritten in the database:")
                     convertCSVToSQL(fileToConvert, dbName,tableName)
+            if choice == 'q':
+                exit()
         except ValueError:
-            print("invalid choice. Please select again")
+            print("An error occured. Returning to start...")
             continue
 
     
