8fdd68c5268fbb5946a7bfa0e4fc4333b37dad54
jmcmillian94 <jmcmillian94@aim>
2024-12-16 12:39:21 -0500

    updated

--- testfile2.py
+++ testfile2.py
@@ -13,7 +13,12 @@ import os
 # function that takes a file and converts it to a SQL table 
 # *right now only works with excel file, need to add csv functionality
 def convertFileToSQLiteTable(fileToConvert,dbName,tableName):
-    df = pd.read_excel(fileToConvert)
+    if fileToConvert.lower().endswith(('.xlsx', '.xls')):
+        df = pd.read_excel(fileToConvert)
+        
+    else:
+        df = pd.read_csv(fileToConvert)
+    
     dbConnect = sqlite3.connect(dbName)
     df.to_sql(tableName, dbConnect, if_exists='replace', index=False)
     dbConnect.close()
@@ -25,22 +30,22 @@ def convertFileToSQLiteTable(fileToConvert,dbName,tableName):
 #############################
 
 #print intro text
-print("SQL Database Creation Tool")
+print("SQL Database Creation Tool\n")
 
-print("This program will take any Excel or CSV file and convert it to a table for the specified SQLite database.")
+print("This program will take any Excel or CSV file and convert it to a table for the specified SQLite database.\n")
 
-dbName = input("Please enter the filepath and name for the database you would like to use (either a new db or an existing one):")
+dbName = input("Please enter the filepath and name for the database you would like to use (either a new db or an existing one):\n")
 
 while True: #start an infinite loop for adding multiple tables
     while True:
-        fileToConvert = input("Please enter the filepath for the excel or csv file you would like to convert to a table:")
+        fileToConvert = input("Please enter the filepath for the excel or csv file you would like to convert to a table:\n")
         if os.path.exists(fileToConvert):
             break
         else:
             print('invalid file path. please try again.')
 
         
-    tableName = input("Please enter a name for the table (existing tables of the same name will be overwritten!):")
+    tableName = input("Please enter a name for the table (existing tables of the same name will be overwritten!):\n")
     convertFileToSQLiteTable(fileToConvert,dbName,tableName)
         
     menuChoice = input("Would you like to add another table to the database? y/n\n")
